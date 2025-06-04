5cdc0898e31d3b10ca5d4f4bbc81d84d047abfd5
Manna Kong <mkong@redhat.com>
2025-06-04 03:02:45 -0500

    Configured patternfly and tanstack router
    Signed-off-by: Manna Kong <mkong@redhat.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    react(),
+    // ...,
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // This line maps '@/' to your 'src' directory
+      // You might have other aliases too
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: { chunkSizeWarningLimit: 2000 },
+});
